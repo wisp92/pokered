5bbe52294f84a7f77394c810058b80502a98ff01
jojobear13 <joseph.billo@gmail.com>
2020-05-27 21:15:31 -0500

    added home pal update functions and freed-up rom 0 space

--- home.asm
+++ home.asm
@@ -4575,6 +4575,88 @@ Delay3::
 	ld c, 3
 	jp DelayFrames
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;gbcnote - new functions
+UpdateAllPal::
+	call UpdateGBCPal_BGP
+UpdateOBPPal::
+	call UpdateGBCPal_OBP0
+	call UpdateGBCPal_OBP1
+	ret
+	
+UpdateGBCPal_BGP::
+	push af
+	ld a, [hGBC]
+	and a
+	jr z, .notGBC
+	push bc
+	push de
+	push hl
+	ld a, [rBGP]
+	ld b, a
+	ld a, [wLastBGP]
+	cp b
+	jr z, .noChangeInBGP
+	callba _UpdateGBCPal_BGP
+.noChangeInBGP
+	pop hl
+	pop de
+	pop bc
+.notGBC
+	pop af
+	ret
+
+UpdateGBCPal_OBP0::
+	push af
+	ld a, [hGBC]
+	and a
+	jr z, .notGBC
+	push bc
+	push de
+	push hl
+	ld a, [rOBP0]
+	ld b, a
+	ld a, [wLastOBP0]
+	cp b
+	jr z, .noChangeInOBP0
+	ld b, BANK(_UpdateGBCPal_OBP)
+	ld hl, _UpdateGBCPal_OBP
+	ld c, CONVERT_OBP0
+	call Bankswitch
+.noChangeInOBP0
+	pop hl
+	pop de
+	pop bc
+.notGBC
+	pop af
+	ret
+
+UpdateGBCPal_OBP1::
+	push af
+	ld a, [hGBC]
+	and a
+	jr z, .notGBC
+	push bc
+	push de
+	push hl
+	ld a, [rOBP1]
+	ld b, a
+	ld a, [wLastOBP1]
+	cp b
+	jr z, .noChangeInOBP1
+	ld b, BANK(_UpdateGBCPal_OBP)
+	ld hl, _UpdateGBCPal_OBP
+	ld c, CONVERT_OBP1
+	call Bankswitch
+.noChangeInOBP1
+	pop hl
+	pop de
+	pop bc
+.notGBC
+	pop af
+	ret	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 GBPalNormal::
 ; Reset BGP and OBP0.
 	ld a, %11100100 ; 3210
